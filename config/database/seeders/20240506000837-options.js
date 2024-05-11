55a511a44f53c51603be71acce75659d8ca4cf91
HelioJr548 <heliojr548@gmail.com>
2024-05-11 20:38:24 -0300

    Atualizando seed: options

--- config/database/seeders/20240506000837-options.js
+++ config/database/seeders/20240506000837-options.js
@@ -5,32 +5,51 @@ module.exports = {
 	up: async (queryInterface, Sequelize) => {
 		await queryInterface.bulkInsert('options', [
 			{
+				form_id: 1,
 				question_id: 1,
 				text: 'Vermelho',
 				created_at: new Date(),
 				updated_at: new Date(),
 			},
 			{
+				form_id: 1,
 				question_id: 1,
 				text: 'Azul',
 				created_at: new Date(),
 				updated_at: new Date(),
 			},
 			{
+				form_id: 1,
 				question_id: 1,
 				text: 'Verde',
 				created_at: new Date(),
 				updated_at: new Date(),
 			},
 			{
+				form_id: 1,
 				question_id: 2,
 				text: 'Cachorro',
 				created_at: new Date(),
 				updated_at: new Date(),
 			},
 			{
+				form_id: 1,
 				question_id: 2,
 				text: 'Gato',
+				created_at: new Date(),
+				updated_at: new Date(),
+			},
+			{
+				form_id: 2,
+				question_id: 2,
+				text: 'Cavalo',
+				created_at: new Date(),
+				updated_at: new Date(),
+			},
+			{
+				form_id: 2,
+				question_id: 2,
+				text: 'Passarinho',
 				created_at: new Date(),
 				updated_at: new Date(),
 			},
